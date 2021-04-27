22540fc19c86bcd1fdf0f6ed1a1c0f67fbcfbe19
Rafael Vasconcelos <rafael.vasconcelos@itenabler.com.br>
2021-04-27 15:48:43 -0300

    passsando a formatação do IMC para toda a tabela

--- js/principal.js
+++ js/principal.js
@@ -2,29 +2,40 @@ var titulo = document.querySelector(".titulo");
 
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente");
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
-var tdImc = paciente.querySelector(".info-imc");
+var pacientes = document.querySelectorAll(".paciente");
 
-var pesoEhValido = true;
-var alturaEhValida = true;
+for(var i=0 ; i < pacientes.length; i++){
+    var paciente = pacientes[i];
 
-if(peso<=0 || peso>=1000){
-    console.log("Peso inválido.");
-    pesoEhValido = false;
-    tdImc.textContent = "Peso inválido";
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+    var tdImc = paciente.querySelector(".info-imc");
+
+    var pesoEhValido = true;
+    var alturaEhValida = true;
+
+    if(peso<=0 || peso>=1000){
+        console.log("Peso inválido.");
+        pesoEhValido = false;
+        tdPeso.textContent = "Peso inválido";
+    }
+
+    if(altura<=0 || altura>=3){
+        console.log("Altura inválida!");
+        alturaEhValida = false;
+        tdAltura.textContent = "Altura inválida";
+    }
+
+    if(pesoEhValido==true && alturaEhValida==true){
+        var imc =peso/(altura*altura);
+        tdImc.textContent = imc.toFixed(2);
+    }else{
+        tdImc.textContent = "Altura e/ou peso inválidos."
+    }
 }
 
-if(altura<=0 || altura>=3){
-    console.log("Altura inválida!");
-    alturaEhValida = false;
-    tdImc.textContent = "Altura inválida";
-}
 
-if(pesoEhValido==true && alturaEhValida==true){
-    var imc =peso/(altura*altura);
-    tdImc.textContent = imc;
-}
+
+
