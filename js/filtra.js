47034e32190ca79a7b68b4d7d34a2e6c213c9402
Rafael Vasconcelos <rafael.vasconcelos@itenabler.com.br>
2021-04-28 15:30:51 -0300

    refinando mecanismo de busca

--- js/filtra.js
+++ js/filtra.js
@@ -1,7 +1,6 @@
 var campoFiltro = document.querySelector("#filtrar-tabela");
 
 campoFiltro.addEventListener("input", function(){
-    console.log(this.value);
     var pacientes = document.querySelectorAll(".paciente");
 
     if(this.value.length>0){
@@ -9,15 +8,16 @@ campoFiltro.addEventListener("input", function(){
             var paciente = pacientes[i];
             var tdNome = paciente.querySelector(".info-nome");
             var nome = tdNome.textContent;
+            var expressao = new RegExp(this.value,"i");
             
-            if(nome != this.value){
+            if(!expressao.test(nome)){
                 paciente.classList.add("invisivel");
             }else{
                 paciente.classList.remove("invisivel"); 
             }
         }
     }else{
-        for(var i = 0; i <pacientes.length;i++){
+        for(var i = 0; i < pacientes.length ; i++ ){
             var paciente = pacientes[i];
             paciente.classList.remove("invisivel");
         }
